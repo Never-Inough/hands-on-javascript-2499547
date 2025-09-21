0bd483d9e82dfa6a31a50e046884fc634c1ae155
teodor <teodor.daraktchiev@dxc.com>
2025-09-21 10:24:13 +0300

    removed comments

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: process.env.WEATHER_API_TOKEN, //replace with quotes
+  appid: process.env.WEATHER_API_TOKEN,
   units: "metric",
   location: "sofia,bg"
 };
